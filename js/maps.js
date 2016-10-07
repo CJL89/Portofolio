c126427cd833adba04a07bf8c682549fb555ce6e
Cesar Luis Jaitman Labaton <cesarjaitmanlabaton@gmail.com>
2016-10-07 09:44:34 -0400

    Set up of JS and CSS file

---
+++ js/maps.js
@@ -0,0 +1,10 @@
+
+//Loading of map, calling it through the function and inputing it in the mad id
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
